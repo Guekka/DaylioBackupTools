faf94863c6f0f403c9bf71fd3b7fb6453670d6d1
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-01-24 09:31:45 +0100

    refactor(merging): split into several functions

--- src/merge.rs
+++ src/merge.rs
@@ -39,25 +39,140 @@ impl PartialEq for Tag {
     }
 }
 
-fn change_mood_id(day_entries: &mut [DayEntry], mood: &mut CustomMood, new_id: i64) {
-    for entry in day_entries {
-        if entry.mood == mood.id {
-            entry.mood = new_id;
-        }
-    }
-    mood.id = new_id
-}
-
-fn change_tag_id(day_entries: &mut [DayEntry], tag: &mut Tag, new_id: i64) {
-    for entry in day_entries {
-        for i in 0..entry.tags.len() {
-            if entry.tags[i] == tag.id {
-                entry.tags[i] = new_id;
-                break;
+impl Daylio {
+    fn change_mood_id(day_entries: &mut [DayEntry], mood: &mut CustomMood, new_id: i64) {
+        for entry in day_entries {
+            if entry.mood == mood.id {
+                entry.mood = new_id;
             }
         }
+        mood.id = new_id
     }
-    tag.id = new_id;
+
+    fn change_tag_id(day_entries: &mut [DayEntry], tag: &mut Tag, new_id: i64) {
+        for entry in day_entries {
+            for i in 0..entry.tags.len() {
+                if entry.tags[i] == tag.id {
+                    entry.tags[i] = new_id;
+                    break;
+                }
+            }
+        }
+        tag.id = new_id;
+    }
+
+    fn make_ids_distinct(&mut self, gen: &mut IdGenerator) {
+        for mood in &mut self.custom_moods {
+            Daylio::change_mood_id(&mut self.day_entries, mood, gen.next());
+        }
+
+        for tag in &mut self.tags {
+            Daylio::change_tag_id(&mut self.day_entries, tag, gen.next());
+        }
+    }
+
+    fn remove_duplicates(&mut self) {
+        // for moods
+        self.custom_moods
+            .sort_by_key(|x| (x.custom_name.to_lowercase(), x.icon_id));
+
+        for i in 1..self.custom_moods.len() {
+            if self.custom_moods[i - 1] == self.custom_moods[i] {
+                let new_id = self.custom_moods[i - 1].id;
+                Daylio::change_mood_id(&mut self.day_entries, &mut self.custom_moods[i], new_id);
+                self.custom_moods[i].id = -1; // mark for deletion
+            }
+        }
+
+        self.custom_moods.retain(|mood| mood.id != -1);
+
+        // for tags
+        self.tags.sort_by_key(|x| (x.name.to_lowercase(), x.icon));
+
+        for i in 1..self.tags.len() {
+            if self.tags[i - 1] == self.tags[i] {
+                let new_id = self.tags[i - 1].id;
+                Daylio::change_tag_id(&mut self.day_entries, &mut self.tags[i], new_id);
+                self.tags[i].id = -1; // mark for deletion
+            }
+        }
+
+        self.tags.retain(|tag| tag.id != -1);
+
+        // for entries
+        self.day_entries
+            .sort_by_key(|x| (x.datetime, x.year, x.month));
+
+        for i in 1..self.day_entries.len() {
+            // we do not want to lose any data, so they need to be exactly the same
+            if self.day_entries[i - 1] == self.day_entries[i] {
+                self.day_entries[i].id = -1; // mark for deletion
+            }
+        }
+
+        self.day_entries.retain(|entry| entry.id != -1);
+    }
+
+    pub fn sanitize(&mut self) {
+        // finally, sort by date and update ids
+        self.custom_moods
+            .sort_by_key(|x| (x.mood_group_id, x.created_at));
+        self.tags.sort_by_key(|x| x.created_at);
+        self.day_entries
+            .sort_by_key(|x| (-x.datetime, -x.year, -x.month));
+
+        // fix: sometimes custom moods have a custom
+        // name and a predefined name
+        // we keep custom name and remove predefined name
+        for mood in self.custom_moods.iter_mut() {
+            if mood.predefined_name_id != -1 && !mood.custom_name.is_empty() {
+                mood.predefined_name_id = -1;
+            }
+        }
+
+        // ids start at 1
+        // first handle predefined moods
+        for mood in self.custom_moods.iter_mut() {
+            if mood.predefined_name_id != -1 {
+                Daylio::change_mood_id(&mut self.day_entries, mood, mood.predefined_name_id);
+            }
+        }
+
+        let mut id_generator = IdGenerator::with_start(1, 6);
+
+        // then handle custom moods
+        // order is important, so we need to sort by mood_group_id and predefined comes first
+        self.custom_moods
+            .sort_by_key(|x| (x.mood_group_id, -x.predefined_name_id));
+        for mood in self.custom_moods.iter_mut() {
+            if mood.predefined_name_id == -1 {
+                Daylio::change_mood_id(&mut self.day_entries, mood, id_generator.next());
+            }
+        }
+
+        // each mood_group_id has an order, so we need to update it
+        for i in 0..self.custom_moods.len() {
+            if i == 0
+                || self.custom_moods[i].mood_group_id != self.custom_moods[i - 1].mood_group_id
+            {
+                self.custom_moods[i].mood_group_order = 0;
+            } else {
+                self.custom_moods[i].mood_group_order =
+                    self.custom_moods[i - 1].mood_group_order + 1;
+            }
+        }
+
+        let mut id_generator = IdGenerator::new(1);
+        for (i, mut tag) in self.tags.iter_mut().enumerate() {
+            Daylio::change_tag_id(&mut self.day_entries, tag, id_generator.next());
+            tag.order = i as i64 + 1;
+        }
+
+        let mut id_generator = IdGenerator::new(1);
+        for entry in self.day_entries.iter_mut() {
+            entry.id = id_generator.next();
+        }
+    }
 }
 
 pub fn merge(mut daylio1: Daylio, mut daylio2: Daylio) -> Daylio {
@@ -65,134 +180,23 @@ pub fn merge(mut daylio1: Daylio, mut daylio2: Daylio) -> Daylio {
 
     // first_pass: make sure we don't have any duplicates id
     let mut id_generator = IdGenerator::new(BIG_OFFSET as i64);
-    for daylio in [&mut daylio1, &mut daylio2].iter_mut() {
-        for mood in &mut daylio.custom_moods {
-            change_mood_id(&mut daylio.day_entries, mood, id_generator.next());
-        }
+    daylio1.make_ids_distinct(&mut id_generator);
+    daylio2.make_ids_distinct(&mut id_generator);
 
-        for tag in &mut daylio.tags {
-            change_tag_id(&mut daylio.day_entries, tag, id_generator.next());
-        }
-    }
-
-    let mut merged = daylio1.clone();
+    let mut merged = daylio1;
     merged
         .custom_moods
         .append(&mut daylio2.custom_moods.clone());
     merged.tags.append(&mut daylio2.tags.clone());
     merged.day_entries.append(&mut daylio2.day_entries.clone());
 
-    // second_pass: make sure we don't have any duplicate
-
-    // for moods
-    merged
-        .custom_moods
-        .sort_by_key(|x| (x.custom_name.to_lowercase(), x.icon_id));
-
-    for i in 1..merged.custom_moods.len() {
-        if merged.custom_moods[i - 1] == merged.custom_moods[i] {
-            let new_id = merged.custom_moods[i - 1].id;
-            change_mood_id(&mut merged.day_entries, &mut merged.custom_moods[i], new_id);
-            merged.custom_moods[i].id = -1; // mark for deletion
-        }
-    }
-
-    merged.custom_moods.retain(|mood| mood.id != -1);
-
-    // for tags
-    merged.tags.sort_by_key(|x| (x.name.to_lowercase(), x.icon));
-
-    for i in 1..merged.tags.len() {
-        if merged.tags[i - 1] == merged.tags[i] {
-            let new_id = merged.tags[i - 1].id;
-            change_tag_id(&mut merged.day_entries, &mut merged.tags[i], new_id);
-            merged.tags[i].id = -1; // mark for deletion
-        }
-    }
-
-    merged.tags.retain(|tag| tag.id != -1);
-
-    // for entries
-    merged
-        .day_entries
-        .sort_by_key(|x| (x.datetime, x.year, x.month));
-
-    for i in 1..merged.day_entries.len() {
-        // we do not want to lose any data, so they need to be exactly the same
-        if merged.day_entries[i - 1] == merged.day_entries[i] {
-            merged.day_entries[i].id = -1; // mark for deletion
-        }
-    }
-
-    merged.day_entries.retain(|entry| entry.id != -1);
-
-    // finally, sort by date and update ids
-    merged
-        .custom_moods
-        .sort_by_key(|x| (x.mood_group_id, x.created_at));
-    merged.tags.sort_by_key(|x| x.created_at);
-    merged
-        .day_entries
-        .sort_by_key(|x| (-x.datetime, -x.year, -x.month));
-
-    // fix: sometimes custom moods have a custom
-    // name and a predefined name
-    // we keep custom name and remove predefined name
-    for mood in merged.custom_moods.iter_mut() {
-        if mood.predefined_name_id != -1 && !mood.custom_name.is_empty() {
-            mood.predefined_name_id = -1;
-        }
-    }
-
-    // ids start at 1
-    // first handle predefined moods
-    for mood in merged.custom_moods.iter_mut() {
-        if mood.predefined_name_id != -1 {
-            change_mood_id(&mut merged.day_entries, mood, mood.predefined_name_id);
-        }
-    }
-
-    let mut id_generator = IdGenerator::with_start(1, 6);
-
-    // then handle custom moods
-    // order is important, so we need to sort by mood_group_id and predefined comes first
-    merged
-        .custom_moods
-        .sort_by_key(|x| (x.mood_group_id, -x.predefined_name_id));
-    for mood in merged.custom_moods.iter_mut() {
-        if mood.predefined_name_id == -1 {
-            change_mood_id(&mut merged.day_entries, mood, id_generator.next());
-        }
-    }
-
-    // each mood_group_id has an order, so we need to update it
-    for i in 0..merged.custom_moods.len() {
-        if i == 0
-            || merged.custom_moods[i].mood_group_id != merged.custom_moods[i - 1].mood_group_id
-        {
-            merged.custom_moods[i].mood_group_order = 0;
-        } else {
-            merged.custom_moods[i].mood_group_order =
-                merged.custom_moods[i - 1].mood_group_order + 1;
-        }
-    }
-
-    let mut id_generator = IdGenerator::new(1);
-    for (i, mut tag) in merged.tags.iter_mut().enumerate() {
-        change_tag_id(&mut merged.day_entries, tag, id_generator.next());
-        tag.order = i as i64 + 1;
-    }
-
-    let mut id_generator = IdGenerator::new(1);
-    for entry in merged.day_entries.iter_mut() {
-        entry.id = id_generator.next();
-    }
+    merged.remove_duplicates();
+    merged.sanitize();
 
     // update metadata
     merged.metadata.number_of_entries = merged.day_entries.len() as i64;
-    merged.metadata.number_of_photos =
-        daylio1.metadata.number_of_photos + daylio2.metadata.number_of_photos;
-    merged.metadata.photos_size = daylio1.metadata.photos_size + daylio2.metadata.photos_size;
+    merged.metadata.number_of_photos += daylio2.metadata.number_of_photos;
+    merged.metadata.photos_size += daylio2.metadata.photos_size;
 
     merged
 }
